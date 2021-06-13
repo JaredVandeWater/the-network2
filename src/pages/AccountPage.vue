64840062f9e91d3cc705c09c96888ebdef14785c
Jared VandeWater <jaredvande@gmail.com>
2021-06-13 02:08:22 -0600

    bummer

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -1,19 +1,58 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
+  <div v-if="state.profile" class="container">
+    <div class="row text-center justify-content-center">
+      <h1>Welcome {{ state.account.name }}, change some settings!</h1>
+    <!-- <img class="rounded" :src="account.picture" alt="" /> -->
+    <!-- <p>{{ account.email }}</p> -->
+    </div>
+    <div @submit.prevent="editData" class="form">
+      <input v-model="state.newData.bio" placeholder="Bio..." type="text">
+      <input v-model="state.newData.class" placeholder="Class..." type="text">
+      <input v-model="state.newData.coverImg" placeholder="Banner..." type="text">
+      <input v-model="state.newData.github" placeholder="Github..." type="text">
+      <input v-model="state.newData.graduated" type="checkbox">
+      <input v-model="state.newData.linkedin" placeholder="LinkedIn..." type="text">
+      <input v-model="state.newData.name" placeholder="Name..." type="text">
+      <input v-model="state.newData.picture" placeholder="Photo..." type="text">
+      <button type="submit">
+        Save
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, onMounted, reactive } from 'vue'
 import { AppState } from '../AppState'
+import { profileService } from '../services/ProfileService'
+import Notification from '../utils/Notification'
 export default {
   name: 'Account',
   setup() {
+    onMounted(async() => {
+      try {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+
+        await profileService.getProfileInfo(state.account.id)
+        console.log(state.account, 'acc')
+        console.log(AppState.profileInfo)
+      } catch (error) {
+        Notification.toast(error, 'error')
+      }
+    })
+    const state = reactive({
+      account: computed(() => AppState.account),
+      profile: computed(() => AppState.profileInfo),
+      newData: {
+
+      }
+    })
     return {
-      account: computed(() => AppState.account)
+      state,
+      editData() {
+        profileService.editData(state.account.id, state.newData)
+        console.log(state.account.id, 'id', state.newData, 'data')
+      }
     }
   }
 }
--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -5,7 +5,11 @@ class ProfileService {
   async getProfileInfo(id) {
     const res = await api.get(`/profiles/${id}`)
     AppState.profileInfo = res.data
-    console.log(AppState.profileInfo)
+    console.log(AppState.profileInfo, 'yes')
+  }
+
+  async editData(id, newData) {
+    await api.post(`/profiles/${id}`, newData)
   }
 }
 export const profileService = new ProfileService()
